f3d15e99946ce7a8e018fe714be6dc3e1859d875
wesleylkm <wesleylkm@outlook.com>
2022-05-06 16:15:28 +0800

    Add: vite react plugin, react 18 dep

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,16 @@
-console.log("lottie-react");
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+const container = document.getElementById("root");
+
+// ! is use here, because I can sure that root exist in the html file
+const root = ReactDOM.createRoot(container!);
+
+// We need StrictMode here, so our code can work with <OffScreen />
+// And follow best practice
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
