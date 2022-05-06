c507032225fd27bdf4df6bd23ba35f12409630b4
wesleylkm <wesleylkm@outlook.com>
2022-05-06 17:07:06 +0800

    Add: Current implementation of lottie-react

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import { Player } from "@lottiefiles/react-lottie-player";
-import animationFile from "./assets/guitarist.json";
+import CurrentPlayer from "./components/CurrentPlayer";
 
 const App = () => {
   return (
     <div>
       <h1>react-lottie</h1>
-      <Player src={animationFile} autoplay loop />
+      <CurrentPlayer />
     </div>
   );
 };
