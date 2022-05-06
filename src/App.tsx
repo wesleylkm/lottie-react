521ad2f40288c5839e6f459095e2a36ef6c4b434
wesleylkm <wesleylkm@outlook.com>
2022-05-06 16:26:19 +0800

    Add: try current react-lottie-player (written in class)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { Player } from "@lottiefiles/react-lottie-player";
+import animationFile from "./assets/guitarist.json";
+
 const App = () => {
   return (
     <div>
       <h1>react-lottie</h1>
+      <Player src={animationFile} autoplay loop />
     </div>
   );
 };
